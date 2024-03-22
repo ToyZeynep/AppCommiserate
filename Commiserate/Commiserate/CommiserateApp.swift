ca872421bd7c8cf0aab135d00d38f3b402baf9ad
Zeynep TOY <zeynep.toy@btpro.net>
2024-03-22 22:26:37 +0300

    Create xcode project

---
+++ Commiserate/Commiserate/CommiserateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommiserateApp.swift
+//  Commiserate
+//
+//  Created by Zeynep Toy on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CommiserateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
